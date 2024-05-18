4c76d67af57dd70cfbc82b70684ddf84c3d19674
DigioMatthy <mattyas2015@gmail.com>
2024-05-18 17:15:48 +0200

    biggest update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <title>BiblioTech</title>
 </head>
 <body>
-    <nav class="sidebar">
+    <nav class="sidebar close">
         <header>
             <div class="image-text">
                 <span class="image">
@@ -20,8 +20,6 @@
                     <span class="name">BiblioTech</span>
                 </div>
             </div>
-
-            <i class='bx bx-chevron-right toggle' ></i>
         </header>
 
         <div class="menu-bar">
@@ -30,12 +28,62 @@
                     <li class="nav-link">
                         <a href="#">
                             <i class='bx bx-home-alt icon' ></i>
-                            <span class="text nav-text">Dashboard</span>
+                            <span class="text nav-text">Scopri</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bxs-book-content icon'></i>
+                            <span class="text nav-text">Categoria</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-library icon'></i>
+                            <span class="text nav-text">La Mia Libreria</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-heart icon'></i>
+                            <span class="text nav-text">Preferiti</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-camera-movie icon'></i>
+                            <span class="text nav-text">Film</span>
                         </a>
                     </li>
                 </ul>
             </div>
+
+            <div class="bottom-content">
+                <li class="">
+                    <a href="#">
+                        <i class='bx bx-log-out icon'></i>
+                        <span class="text nav-text">Logout</span>
+                    </a>
+                </li>
+
+                <li class="mode">
+                    <div class="moon-sun">
+                        <i class='bx bx-moon icon moon'></i>
+                        <i class='bx bx-sun icon sun'></i>
+                    </div>
+                    <span class="mode-text text">Dark Mode</span>
+
+                    <div class="toggle-switch">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+            </div>
         </div>
     </nav>
+<script src="script.js"></script>
+
+<section class="home">
+    <div class="text">CIAOOO</div>
+</section>
 </body>
 </html>
